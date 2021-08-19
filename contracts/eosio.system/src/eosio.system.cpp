5cf114da20ec772041c793dcd457294bea674fd9
joshglogau <josh.glogau@siliconswamp.io>
2021-08-19 15:48:43 -0400

    remove 12 char requirement

--- contracts/eosio.system/src/eosio.system.cpp
+++ contracts/eosio.system/src/eosio.system.cpp
@@ -324,28 +324,28 @@ namespace eosiosystem {
                             ignore<authority> owner,
                             ignore<authority> active ) {
 
-      if( creator != get_self() ) {
-         uint64_t tmp = newact.value >> 4;
-         bool has_dot = false;
+      // if( creator != get_self() ) {
+      //    uint64_t tmp = newact.value >> 4;
+      //    bool has_dot = false;
 
-         for( uint32_t i = 0; i < 12; ++i ) {
-           has_dot |= !(tmp & 0x1f);
-           tmp >>= 5;
-         }
-         if( has_dot ) { // or is less than 12 characters
-            auto suffix = newact.suffix();
-            if( suffix == newact ) {
-               name_bid_table bids(get_self(), get_self().value);
-               auto current = bids.find( newact.value );
-               check( current != bids.end(), "no active bid for name" );
-               check( current->high_bidder == creator, "only highest bidder can claim" );
-               check( current->high_bid < 0, "auction for name is not closed yet" );
-               bids.erase( current );
-            } else {
-               check( creator == suffix, "only suffix may create this account" );
-            }
-         }
-      }
+      //    for( uint32_t i = 0; i < 12; ++i ) {
+      //      has_dot |= !(tmp & 0x1f);
+      //      tmp >>= 5;
+      //    }
+      //    if( has_dot ) { // or is less than 12 characters
+      //       auto suffix = newact.suffix();
+      //       if( suffix == newact ) {
+      //          name_bid_table bids(get_self(), get_self().value);
+      //          auto current = bids.find( newact.value );
+      //          check( current != bids.end(), "no active bid for name" );
+      //          check( current->high_bidder == creator, "only highest bidder can claim" );
+      //          check( current->high_bid < 0, "auction for name is not closed yet" );
+      //          bids.erase( current );
+      //       } else {
+      //          check( creator == suffix, "only suffix may create this account" );
+      //       }
+      //    }
+      // }
 
       user_resources_table  userres( get_self(), newact.value );
 
